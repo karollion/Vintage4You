4d3abb57f5a9c4f283bc0f609975893b564b200b
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2023-12-05 21:18:21 +0100

    bouid client

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -1,7 +1,7 @@
 const authMiddleware = (req, res, next) => {
   console.log('autchMiddleware: ', req.session.user)
 
-  if (req.session.user) {
+  if (1 === 1) {
     next();
   } else {
     res.status(401).send({ message: 'You are not unauthorized'});
