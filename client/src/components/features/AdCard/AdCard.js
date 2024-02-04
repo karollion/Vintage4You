e176c9cc78104974e32311cfabaaab0d21a6fa65
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-02-04 10:12:28 +0100

    Add styles to ad card

--- client/src/components/features/AdCard/AdCard.js
+++ client/src/components/features/AdCard/AdCard.js
@@ -2,21 +2,27 @@ import styles from './AdCard.module.scss';
 import { Link } from 'react-router-dom';
 import { Card, Col, Button } from 'react-bootstrap';
 import { IMGS_URL } from '../../../config';
+import PropTypes from 'prop-types'
 
 const AdCard = ({ ad }) => {
 
   return (
     <Col xs='12' md='6' lg='4' className='mb-4'>
-      <Card className='p-3'>
-        <Card.Img variant='top' src={IMGS_URL + ad.picture} className={styles.img} />
-			  <Card.Body>
-          <Card.Title>{ad.title}</Card.Title>
-          <Card.Text>{ad.location}</Card.Text>
+      <div className={styles.card}>
+        <img variant='top' alt='ad_image' src={IMGS_URL + ad.picture} className={styles.img} />
+			  <div className={styles.body}>
+          <h3>{ad.title}</h3>
+          <p>Location: {ad.location}</p>
+          <p>Date added: {ad.date.substring(0, 10)}</p>
           <Button variant="primary" as={Link} to={"/ad/" + ad._id}>Read more</Button>
-        </Card.Body>
-      </Card>
+        </div>
+      </div>
     </Col>
   );
 };
+
+AdCard.propTypes = {
+	ad: PropTypes.object.isRequired,
+}
 
 export default AdCard;
