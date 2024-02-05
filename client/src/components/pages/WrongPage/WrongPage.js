55520be1103f9ff551fe12d5816f67b47337b71b
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2024-02-05 21:07:16 +0100

    Add color to text

--- client/src/components/pages/WrongPage/WrongPage.js
+++ client/src/components/pages/WrongPage/WrongPage.js
@@ -1,11 +1,24 @@
-//import styles from './WrongPage.module.scss'
+import { useNavigate } from 'react-router-dom';
+import Button from '../../common/Button/Button';
+import styles from './WrongPage.module.scss'
+import Title from '../../common/Title/Title';
 
 const WrongPage = () => {
-    return (
-      <div className='min-vh-100'>
-        404 Page not found
-      </div>
-    );
+  const navigate = useNavigate();
+
+  const handleBack = e => {
+    e.preventDefault();
+    navigate('/');
+  }
+  return (
+    <div className={styles.root}>
+      <div className={styles.vaves}></div> 
+      <Title>404</Title>
+      <h4>Oh no! It seems that the page you are looking for is hidden somewhere. Let's try to get back to our home page.</h4>
+      <Button className={styles.btn} action={handleBack}>Back to home</Button>
+
+    </div>
+  );
 };
 
 export default WrongPage;
