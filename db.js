9f9749460de3c502ebc62f77d68927fd62d819ae
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-03-22 21:39:20 +0100

    change db url

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const connectToDB = () => {
   const NODE_ENV = process.env.NODE_ENV;
   let dbUri = '';
  
-  if(NODE_ENV === 'production') dbUri = `mongodb+srv://${user}:${password}@cluster0.pbd1wk2.mongodb.net/Ads_Board_DB?retryWrites=true&w=majority``;
+  if(NODE_ENV === 'production') dbUri = `mongodb+srv://${user}:${password}@cluster0.pbd1wk2.mongodb.net/Ads_Board_DB?retryWrites=true&w=majority`;
   else if(NODE_ENV === 'test') dbUri = 'mongodb://0.0.0.0:27017/AdsBoardDBtest';
   else dbUri = 'mongodb://0.0.0.0:27017/AdsBoardDB';
 
